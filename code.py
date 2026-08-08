554f8b532d3afcf984eaae2e25f9bcf94394472e
Mayuri Rai <mayurirai2084@gmail.com>
2026-08-08 20:18:14 +0530

    initial commit

--- code.py
+++ code.py
@@ -1,6 +1,39 @@
-name = "tony Stark"
+"""name = "tony Stark"
 age = 53 
 height = "1.83 m"
 superheroname = input("enter your superhero name :")
 print ("my superhero name is " + superheroname)
-
+age = input("enter you additional age :")
+new_age = int(age) + 1
+print(new_age)
+#age = int(age)
+print (float(new_age))
+print(1+69.8)#type conversion implicit 
+print(1 + int(69.8))#type casting explicit"""
+"""a = int(input("enter a number to be operated"))
+b = int(input("enter another number to be operated"))
+#operant = input ("enter the operation you wanted to perform:")
+sum = a+b 
+print(sum)"""
+# paragraph = "I am a machine learning engineer and i love the job of it and i love to do it effeciently"
+# print(paragraph.upper())
+# print(paragraph.lower())
+# print(paragraph.title())
+# print(paragraph.capitalize())
+# print(paragraph.count("love"))
+# print(paragraph.find("love"))
+# print(paragraph.replace("love ", "so much in love with "))
+# print(paragraph.split(" "))
+# print("love" in paragraph)
+age = int(input("enter you age :"))
+age = int(input("enter your age :"))
+age = int(input ("enter your age :"))
+name = "i am mayuri rai and i am a machine learning engineer AND I LOVE MY JOB "
+print(name.upper())
+print(name.lower())
+print(name.find("love"))
+print(name.count("love"))
+print(name.title())
+print(name.replace("rai","saraf"))
+print(name.capitalize())
+print("love" in name)
